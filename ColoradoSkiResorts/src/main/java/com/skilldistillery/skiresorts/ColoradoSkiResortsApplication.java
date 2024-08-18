f6ffec69024b28efdb3c782a844ae1f3cf5aef09
Adrian <adrian.vidal9589@gmail.com>
2024-08-18 11:42:32 -0600

    Create Boot MVC project, connect JPA project dependency

---
+++ ColoradoSkiResorts/src/main/java/com/skilldistillery/skiresorts/ColoradoSkiResortsApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.skiresorts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColoradoSkiResortsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColoradoSkiResortsApplication.class, args);
+	}
+
+}
